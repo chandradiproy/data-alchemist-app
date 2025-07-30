439a8913930f1709acaf29707b7bae2b26c6ee4b
chandradiproy <official.chandradip@gmail.com>
2025-07-30 23:53:00 +0530

    eslint disabled

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Add this block to disable ESLint during the build
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
